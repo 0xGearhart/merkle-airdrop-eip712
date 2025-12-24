6156c67dc3441444a6c854c79b8a260f9bdf0ed1
gearhart <geardev26@gmail.com>
2025-12-23 16:31:53 -0800

    add initial contracts

---
+++ src/AirdropToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+pragma solidity ^0.8.31;
+
+contract AirdropToken is ERC20 {
+    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) ERC20(_name, _symbol) {
+        _mint(msg.sender, _initialSupply);
+    }
+}
---
+++ src/MerkleAirdrop.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: MIT
+
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+pragma solidity ^0.8.31;
+
+/**
+ * @title MerkleAirdrop
+ * @author Dustin Gearhart
+ * @notice Airdrop claim contract that uses a merkle tree to minimize gas expenditure
+ */
+contract MerkleAirdrop {
+    /*//////////////////////////////////////////////////////////////
+                               LIBRARIES
+    //////////////////////////////////////////////////////////////*/
+    using SafeERC20 for IERC20;
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error MerkleAirdrop__InvalidMerkleProof();
+    error MerkleAirdrop__AlreadyClaimed();
+
+    /*//////////////////////////////////////////////////////////////
+                                 STATE
+    //////////////////////////////////////////////////////////////*/
+    IERC20 private immutable i_airdropToken;
+    bytes32 private immutable i_merkleRoot;
+    mapping(address claimers => bool) private hasClaimed;
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+    event Claim(address indexed account, uint256 amount);
+
+    /*//////////////////////////////////////////////////////////////
+                               FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    constructor(address airdropToken, bytes32 merkleRoot) {
+        i_airdropToken = IERC20(airdropToken);
+        i_merkleRoot = merkleRoot;
+    }
+
+    /**
+     * @notice Claim airdrop for an address
+     * @param account address of account claiming tokens
+     * @param amount amount of tokens to be claimed
+     * @param merkleProof merkle proof to verify account is eligible to claim
+     */
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        // generate leaf
+        bytes32 merkleLeaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        // verify merkle proof
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, merkleLeaf)) {
+            revert MerkleAirdrop__InvalidMerkleProof();
+        }
+        // make sure account has not already claimed
+        if (hasClaimed[account]) {
+            revert MerkleAirdrop__AlreadyClaimed();
+        }
+        // emit and transfer tokens
+        emit Claim(account, amount);
+        i_airdropToken.safeTransfer(account, amount);
+    }
+}
