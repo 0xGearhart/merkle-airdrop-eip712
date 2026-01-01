dc3b9225e44ffc9aa1bc80e020f0dd8982292f1e
gearhart <geardev26@gmail.com>
2026-01-01 14:22:51 -0800

    fix test file

--- test/integration/MerkleAirdropTest.t.sol
+++ test/integration/MerkleAirdropTest.t.sol
@@ -13,15 +13,28 @@ contract MerkleAirdropTest is Test, CodeConstants {
     MerkleAirdrop merkleAirdrop;
     AirdropToken airdropToken;
 
+    bytes32 merkleRoot;
     address user1;
-    address user1PrivKey;
+    uint256 user1PrivKey;
     address user2;
-    address user2PrivKey;
+    uint256 user2PrivKey;
+
+    bytes32[] emptyProof;
+    bytes32[] user1Proof = [
+        bytes32(0x8ebcc963f0588d1ded1ebd0d349946755f27e95d1917f9427a207d8935e04d4b),
+        bytes32(0xf4216065bf6ac971b2f1ba0fef5920345dcaeceabf5f200030a1cd530dd44a89),
+        bytes32(0x2aadccb0553b8c9968b78ca32bb891c1dd527eb553ff5b19aa35560e4757e5b0)
+    ];
+    bytes32[] user2Proof = [
+        bytes32(0xef54b0c83407e0c74021e9c900344391f8b30fb6c98e7689f3c6015840959d08),
+        bytes32(0xf4216065bf6ac971b2f1ba0fef5920345dcaeceabf5f200030a1cd530dd44a89),
+        bytes32(0x2aadccb0553b8c9968b78ca32bb891c1dd527eb553ff5b19aa35560e4757e5b0)
+    ];
 
     function setUp() public {
         deploy = new Deploy();
         (airdropToken, merkleAirdrop) = deploy.run();
-
+        merkleRoot = merkleAirdrop.getMerkleRoot();
         (user1, user1PrivKey) = makeAddrAndKey("user1");
         (user2, user2PrivKey) = makeAddrAndKey("user2");
     }
